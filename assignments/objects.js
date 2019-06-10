63d4506ee0ff99d07ac4f981bcddc83bc96db473
John <cjd8801@gmail.com>
2019-06-10 18:35:38 -0500

    final

--- assignments/objects.js
+++ assignments/objects.js
@@ -78,6 +78,12 @@ console.log(intern2.speak());
 
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
+intern5.multiplyNums = {
+  multiplyNums: function(x,y){
+    const product = x * y;
+  }
+}
+console.log(intern5.multiplyNums(3*4));
 
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
